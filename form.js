580d047353d852d4f9bd628536f774ad4cbddd4b
Khomotso Sekhaolelo <111143532+khomotsojane@users.noreply.github.com>
2023-02-24 01:02:29 +0200

    Added new list of arrays and fixed the video tag

--- form.js
+++ form.js
@@ -76,6 +76,130 @@ function addCountries() {
         "Guinea",
         "Guinea-Bissau",
         "Guyana",
+        "Haiti",
+        "Herzegovina",
+        "Honduras",
+        "Hungary",
+        "Iceland",
+        "India",
+        "Indonesia",
+        "Iran",
+        "Iraq",
+        "Ireland",
+        "Israel",
+        "Italy",
+        "Jamaica",
+        "Japan",
+        "Jordan",
+        "Kazakhstan",
+        "Kenya",
+        "Kiribati",
+        "Korea, North",
+        "Korea, South",
+        "Kosovo",
+        "Kuwait",
+        "Kyrgyzstan",
+        "Laos",
+        "Latvia",
+        "Lebanon",
+        "Lesotho",
+        "Liberia",
+        "Libya",
+        "Liechtenstein",
+        "Lithuania",
+        "Luxembourg",
+        "Madagascar",
+        "Malawi",
+        "Malaysia",
+        "Maldives",
+        "Mali",
+        "Malta",
+        "Marshall Islands",
+        "Mauritania",
+        "Mauritius",
+        "Mexico",
+        "Micronesia, Federated States of",
+        "Moldova",
+        "Monaco",
+        "Mongolia",
+        "Montenegro",
+        "Morocco",
+        "Mozambique",
+        "Myanmar (Burma)",
+        "Namibia",
+        "Nauru",
+        "Nepal",
+        "Netherlands",
+        "New Zealand",
+        "Nicaragua",
+        "Niger",
+        "Nigeria",
+        "North Macedonia",
+        "Norway",
+        "Oman",
+        "Pakistan",
+        "Palau",
+        "Panama",
+        "Papua New Guinea",
+        "Paraguay",
+        "Peru",
+        "Philippines",
+        "Poland",
+        "Portugal",
+        "Qatar",
+        "Romania",
+        "Russia",
+        "Rwanda",
+        "Saint Kitts and Nevis",
+        "Saint Lucia",
+        "Saint Vincent and the Grenadines",
+        "Samoa",
+        "San Marino",
+        "Sao Tome and Principe",
+        "Saudi Arabia",
+        "Senegal",
+        "Serbia",
+        "Seychelles",
+        "Sierra Leone",
+        "Singapore",
+        "Slovakia",
+        "Slovenia",
+        "Solomon Islands",
+        "Somalia",
+        "South Africa",
+        "Spain",
+        "Sri Lanka",
+        "Sudan",
+        "Sudan, South",
+        "Suriname",
+        "Sweden",
+        "Switzerland",
+        "Syria",
+        "Taiwan",
+        "Tajikistan",
+        "Tanzania",
+        "Thailand",
+        "Togo",
+        "Tonga",
+        "Trinidad and Tobago",
+        "Tunisia",
+        "Turkey",
+        "Turkmenistan",
+        "Tuvalu",
+        "Uganda",
+        "Ukraine",
+        "United Arab Emirates",
+        "United Kingdom",
+        "United States",
+        "Uruguay",
+        "Uzbekistan",
+        "Vanuatu",
+        "Vatican City",
+        "Venezuela",
+        "Vietnam",
+        "Yemen",
+        "Zambia",
+        "Zimbabwe"
     ]; 
 
     for (var i = 0; i < countries.length; i++) {
@@ -89,145 +213,7 @@ function addCountries() {
 
 addCountries();
 
-        // Haiti
-        // Herzegovina
-        // Honduras
-        // Hungary
-        
-        
-        // Iceland
-        // India
-        // Indonesia
-        // Iran
-        // Iraq
-        // Ireland
-        // Israel
-        // Italy
-        
-        // Jamaica
-        // Japan
-        // Jordan
-        
-        // Kazakhstan
-        // Kenya
-        // Kiribati
-        // Korea, North
-        // Korea, South
-        // Kosovo
-        // Kuwait
-        // Kyrgyzstan
-        
-        // Laos
-        // Latvia
-        // Lebanon
-        // Lesotho
-        // Liberia
-        // Libya
-        // Liechtenstein
-        // Lithuania
-        // Luxembourg
-        
-        // Madagascar
-        // Malawi
-        // Malaysia
-        // Maldives
-        // Mali
-        // Malta
-        // Marshall Islands
-        // Mauritania
-        // Mauritius
-        // Mexico
-        // Micronesia, Federated States of
-        // Moldova
-        // Monaco
-        // Mongolia
-        // Montenegro
-        // Morocco
-        // Mozambique
-        // Myanmar (Burma)
-        
-        // Namibia
-        // Nauru
-        // Nepal
-        // Netherlands
-        // New Zealand
-        // Nicaragua
-        // Niger
-        // Nigeria
-        // North Macedonia
-        // Norway
-        
-        // Oman
-        
-        // Pakistan
-        // Palau
-        // Panama
-        // Papua New Guinea
-        // Paraguay
-        // Peru
-        // Philippines
-        // Poland
-        // Portugal
-    
-        // Qatar
-        // R
-        // Romania
-        // Russia
-        // Rwanda
-
-        // Saint Kitts and Nevis
-        // Saint Lucia
-        // Saint Vincent and the Grenadines
-        // Samoa
-        // San Marino
-        // Sao Tome and Principe
-        // Saudi Arabia
-        // Senegal
-        // Serbia
-        // Seychelles
-        // Sierra Leone
-        // Singapore
-        // Slovakia
-        // Slovenia
-        // Solomon Islands
-        // Somalia
-        // South Africa
-        // Spain
-        // Sri Lanka
-        // Sudan
-        // Sudan, South
-        // Suriname
-        // Sweden
-        // Switzerland
-        // Syria
-
-        // Taiwan
-        // Tajikistan
-        // Tanzania
-        // Thailand
-        // Togo
-        // Tonga
-        // Trinidad and Tobago
-        // Tunisia
-        // Turkey
-        // Turkmenistan
-        // Tuvalu
-
-        // Uganda
-        // Ukraine
-        // United Arab Emirates
-        // United Kingdom
-        // United States
-        // Uruguay
-        // Uzbekistan
-
-        // Vanuatu
-        // Vatican City
-        // Venezuela
-        // Vietnam
-        // Yemen
-        // Zambia
-        // Zimbabwe
+       
 
 
 
